9e8ad56560f3789db641337ee7bfed3f0abafbb4
Celal Anak <anakcelal@hotmail.com>
2021-08-25 16:45:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace patika.dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.Write("İsminizi girin: ");
+            string name = Console.ReadLine();
+
+            System.Console.Write("Soyisminizi girin: ");
+            string surName = Console.ReadLine();
+
+            System.Console.Write("Merhaba {0} {1}", name,surName);
+
+            Console.ReadKey();
+        }
+    }
+}
